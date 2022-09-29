c8f6b3dffea74bb8776bb56b54c45bac5607d33e
Ikhwanwp <wiratama900@gmail.com>
2022-09-29 10:41:05 +0700

    feat : Integration Detail Movie

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\User\DashboardController;
+use App\Http\Controllers\User\MovieController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,6 +23,8 @@ Route::redirect('/', '/login');
 
 Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+    Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show');
 });
 
 Route::prefix('prototype')->name('prototype.')->group(function () {
